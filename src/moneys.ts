daa08e720d6596cc20ee5ceb17314116b9b6770a
engsr6982 <pwxg286704@163.com>
2023-12-09 21:53:25 +0800

    update moneys

--- src/moneys.ts
+++ src/moneys.ts
@@ -1,7 +1,4 @@
-// Usage:
-// 初次使用请先调用initModule初始化模块
-
-interface _MoneyConfig {
+interface MoneyConfig_ {
     /**开关 */
     Enable: boolean;
     /**经济类型 */
@@ -12,48 +9,21 @@ interface _MoneyConfig {
     MoneyName: string;
 }
 
-interface _Money_Cfg_Cache_Type {
-    /**是否初始化 */
-    init: boolean;
-    Money: _MoneyConfig;
-}
+export default class moneys {
+    private cfg: MoneyConfig_;
 
-export const _Money_Cfg_Cache: _Money_Cfg_Cache_Type = {
-    init: false,
-    Money: {
-        Enable: null,
-        MoneyName: null,
-        ScoreType: null,
-        MoneyType: null,
-    },
-};
-
-export class moneys {
-    /**
-     * 初始化模块
-     * @param moneyConfig 模块配置项
-     * @returns 是否初始化成功
-     */
-    static initModule(moneyConfig: _MoneyConfig): boolean {
-        try {
-            _Money_Cfg_Cache.Money.Enable = moneyConfig.Enable;
-            _Money_Cfg_Cache.Money.MoneyName = moneyConfig.MoneyName;
-            _Money_Cfg_Cache.Money.ScoreType = moneyConfig.ScoreType;
-            _Money_Cfg_Cache.Money.MoneyType = moneyConfig.MoneyType;
-            _Money_Cfg_Cache.init = true;
-            return true;
-        } catch (err) {
-            logger.error(`初始化模块失败 ${err}\n${err.stack}`);
-            return false;
-        }
+    constructor(config: MoneyConfig_) {
+        this.cfg = config;
     }
 
     /**
-     * 模块配置项是否初始化
-     * @returns 是否初始化
+     * 更新配置
+     * @param config 配置
+     * @returns 是否更新成功
      */
-    static isModuleInit(): boolean {
-        return _Money_Cfg_Cache.init;
+    updateConfig(config: MoneyConfig_): boolean {
+        this.cfg = config;
+        return true;
     }
 
     /**
@@ -62,7 +32,7 @@ export class moneys {
      * @param msg 消息内容
      * @returns 是否发送成功
      */
-    static tell(player: Player, msg: string): boolean {
+    private tell(player: Player, msg: string): boolean {
         return player.tell(`[moneys] ${msg}`);
     }
 
@@ -70,9 +40,9 @@ export class moneys {
      * 未知经济（抛出错误）
      * @param player 玩家对象（可选）
      */
-    static unknownMoneyType(player?: Player) {
+    private unknownMoneyType(player?: Player) {
         player ? this.tell(player, `出现了一个错误，导致插件无法完成该操作，请联系服务器管理员。`) : null;
-        throw new Error("未知的经济类型" + _Money_Cfg_Cache.Money.MoneyType);
+        throw new Error("未知的经济类型" + this.cfg.MoneyType);
     }
 
     /**
@@ -80,12 +50,12 @@ export class moneys {
      * @param player 玩家对象
      * @returns 玩家的经济
      */
-    static getPlayeyMoney(player: Player): number {
-        switch (_Money_Cfg_Cache.Money.MoneyType) {
+    getPlayeyMoney(player: Player): number {
+        switch (this.cfg.MoneyType) {
             case "llmoney":
                 return money.get(player.xuid);
             case "score":
-                return player.getScore(_Money_Cfg_Cache.Money.ScoreType);
+                return player.getScore(this.cfg.ScoreType);
             default:
                 this.unknownMoneyType(player);
         }
@@ -97,9 +67,9 @@ export class moneys {
      * @param deMoney 要消耗的经济
      * @returns 字符串
      */
-    static getPlayerMoneyStr(player: Player, deMoney: number) {
-        const playerMoney = _Money_Cfg_Cache.Money.Enable ? this.getPlayeyMoney(player) : 0;
-        return `§l此操作需消耗§9[§e${deMoney}§9]§r§l${_Money_Cfg_Cache.Money.MoneyName}, 当前${_Money_Cfg_Cache.Money.MoneyName}: §a${playerMoney}`;
+    getPlayerMoneyStr(player: Player, deMoney: number) {
+        const playerMoney = this.cfg.Enable ? this.getPlayeyMoney(player) : 0;
+        return `§l此操作需消耗§9[§e${deMoney}§9]§r§l${this.cfg.MoneyName}, 当前${this.cfg.MoneyName}: §a${playerMoney}`;
     }
 
     /**
@@ -108,13 +78,13 @@ export class moneys {
      * @param addMoney 要增加的经济
      * @returns 是否增加成功
      */
-    static addPlayerMoney(player: Player, addMoney: number) {
-        if (_Money_Cfg_Cache.Money.Enable === false) return true;
-        switch (_Money_Cfg_Cache.Money.MoneyType) {
+    addPlayerMoney(player: Player, addMoney: number) {
+        if (this.cfg.Enable === false) return true;
+        switch (this.cfg.MoneyType) {
             case "llmoney":
                 return money.add(player.xuid, addMoney);
             case "score":
-                return player.addScore(_Money_Cfg_Cache.Money.ScoreType, addMoney);
+                return player.addScore(this.cfg.ScoreType, addMoney);
             default:
                 this.unknownMoneyType(player);
         }
@@ -126,20 +96,20 @@ export class moneys {
      * @param deMoney 要扣除的经济
      * @returns 是否扣除成功
      */
-    static deductPlayerMoney(player: Player, deMoney: number) {
-        if (_Money_Cfg_Cache.Money.Enable === false) return true;
+    deductPlayerMoney(player: Player, deMoney: number) {
+        if (this.cfg.Enable === false) return true;
         const cm = this.getPlayeyMoney(player);
         if (cm >= deMoney) {
-            switch (_Money_Cfg_Cache.Money.MoneyType) {
+            switch (this.cfg.MoneyType) {
                 case "llmoney":
                     return money.reduce(player.xuid, deMoney);
                 case "score":
-                    return player.reduceScore(_Money_Cfg_Cache.Money.ScoreType, deMoney);
+                    return player.reduceScore(this.cfg.ScoreType, deMoney);
                 default:
                     this.unknownMoneyType(player);
             }
         }
-        this.tell(player, `${_Money_Cfg_Cache.Money.MoneyName}不足！ 无法继续操作!`);
+        this.tell(player, `${this.cfg.MoneyName}不足！ 无法继续操作!`);
         return false;
     }
 }
